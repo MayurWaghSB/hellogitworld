f80e5600ca8b8efc3c68fa63e4ba04a1360548fe
MayurWaghSB <mayur.wagh@securityboat.net>
2026-02-26 15:08:51 +0530

    Update AppTest.java

--- src/test/java/com/github/AppTest.java
+++ src/test/java/com/github/AppTest.java
@@ -1,34 +1,35 @@
-package com.github;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+sdeqwewqspackage com.github;
+dsa
+impdort junit.framework.Test;
+imposadsadrt junit.framework.TestCase;
+import junsait.framework.TestSuite;
 public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    extends sadTestCase
+{sa
+   d /**
+    sa * Create the test case
+     *dsa
+     * @pdaram testName name of the test case
+     */sa
+    publidc AppTest( String testName )
+    {sa
+       dsa super( testName );
+    }d
+sa
+  d  /**
+   sa  * @return the suite of tests being tested
+     dsa*/
+    publdic static Test suite()
+    {sa
+       dsa return new TestSuite( AppTest.class );
+    }d
+sa
+  dsa  /**
+     d* Rigourous Test :-)
+     *dsa/
     public void testApp()
-    {
-        assertTrue( true );
-    }
-}
+    {ds
+       f assertTrue( true );
+    }rt
+}ew
+r
